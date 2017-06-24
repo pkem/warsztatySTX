7d8bf31e8552308e81f2c6f010a717fb57288aa6
Pawel <pkem@o2.pl>
2017-06-24 13:27:46 +0200

    zadanie 3 niekompletne

---
+++ testy_jedn3.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+class Logowanie (unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+        self.driver.maximize_window()
+
+    def test_login(self):
+        driver = self.driver
+        self.driver.get("http://diabcontrol1.herokuapp.com")
+        self.login('admin', 'admin123')
+        dirver.implicitly_wait(5)
+        self.assertIn('Welcome to DiabControl system', driver.page_source)
+
+        nawigacja=find_elements_by_clas("nav nav-sidebar")
+        nawigacja.find_element_by_tag("a")
+        a=nawigacja.find_element_by_tag("a")
+
+        print [e.text for e in a]
+    def login(self, username, password):
+        driver = self.driver
+        login = driver.find_element_by_name('username')
+        login.clear()
+        login.send_keys(username)
+
+        haslo = driver.find_element_by_name('password')
+        haslo.clear()
+        haslo.send_keys(password)
+        haslo.send_keys(Keys.RETURN)
+
+
+#    def tearDown(self):
+#        self.driver.quit()
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
